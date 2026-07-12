73d7ebb7a6d4c28114a34aeee6215e39f3cf85b9
Veritaserum04 <amruthabhatv@gmail.com>
2026-07-12 20:09:42 +0530

    Configure production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://expense-tracker-backend-k88k.onrender.com/api",
 });
 
 export default api;
